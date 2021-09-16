ba2679e8475efc832d5b354f9d9aebdb8e1af354
Zeta Prawira Syah <86107409+zetaprawira7@users.noreply.github.com>
2021-09-16 13:43:13 +0700

    Do Assignment W2

---
+++ TXT/myscript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# REV00 Thu 16 Sep 2021 13:40:00 WIB
+# START Thu 16 Sep 2021 13:40:00 WIB
+
+FILES="my*.txt my*.sh"
+SHA="SHA256SUM"
+
+echo "rm -f $SHA $SHA.asc"
+rm -f $SHA $SHA.asc
+
+echo "sha256sum $FILES > $SHA"
+sha256sum $FILES > $SHA
+
+echo "sha256sum -c $SHA"
+sha256sum -c $SHA
+
+echo "gpg --output $SHA.asc --armor --sign --detach-sign $SHA"
+gpg --output $SHA.asc --armor --sign --detach-sign $SHA
+
+echo "gpg --verify $SHA.asc $SHA"
+gpg --verify $SHA.asc $SHA
+
+exit 0
